3a76d65b5ec2c0aa48c3fdb92f0d08687de0d9f9
jonsori <yohanees53@gmail.com>
2025-12-29 23:02:11 +0300

    Enable PostgreSQL support for persistent data on Render

--- noticeboard_cms/settings.py
+++ noticeboard_cms/settings.py
@@ -76,6 +76,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'noticeboard_cms.wsgi.application'
 
 
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -85,6 +87,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 
 
 # Password validation
