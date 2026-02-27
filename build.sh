a16c10fca84251dd6d9d03860bce9fb2a6cbd7cb
jonsori <yohanees53@gmail.com>
2026-02-27 21:20:25 +0300

    Configure persistent SQLite using Render Disks

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Ensure media directory exists on persistent disk
+if [ -d "/data" ]; then
+    mkdir -p /data/media/clubs/logos
+    mkdir -p /data/media/notices
+fi
--- noticeboard_cms/settings.py
+++ noticeboard_cms/settings.py
@@ -81,10 +81,17 @@ import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Use /data directory on Render for persistent SQLite, otherwise use BASE_DIR
+RENDER_DATA_DIR = Path('/data')
+if RENDER_DATA_DIR.exists():
+    DB_PATH = RENDER_DATA_DIR / 'db.sqlite3'
+else:
+    DB_PATH = BASE_DIR / 'db.sqlite3'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_PATH,
     }
 }
 
@@ -131,7 +138,10 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+if RENDER_DATA_DIR.exists():
+    MEDIA_ROOT = RENDER_DATA_DIR / 'media'
+else:
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
